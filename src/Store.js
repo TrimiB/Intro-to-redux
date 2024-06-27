b2858f2c4c66620535f6fb635fdb3cafd541325f
Kushtrim <bujupikushtrim@outlook.com>
2024-06-27 22:45:44 +0200

    using thunk with applyMiddleware

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
 import CustomerReducer from './features/customers/customerSlice';
 import AccountReducer from './features/accounts/accountSlice';
 
@@ -7,6 +8,6 @@ const rootReducer = combineReducers({
   customer: CustomerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
