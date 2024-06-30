cdbae4efd9338dfb5d67380e4ade9d986fa96b7c
Kushtrim <bujupikushtrim@outlook.com>
2024-06-30 21:59:37 +0200

    refactoring, using redux toolkit

--- src/Store.js
+++ src/Store.js
@@ -1,14 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { thunk } from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
 import CustomerReducer from './features/customers/customerSlice';
 import AccountReducer from './features/accounts/accountSlice';
 
-const rootReducer = combineReducers({
-  account: AccountReducer,
-  customer: CustomerReducer,
+const store = configureStore({
+  reducer: {
+    account: AccountReducer,
+    customer: CustomerReducer,
+  },
 });
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
